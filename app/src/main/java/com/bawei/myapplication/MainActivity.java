9cc6b6d6aca27c66ee096caea0eb665bf9b48716
hanchong6666 <546321691@qq.com>
2016-01-19 20:00:53 +0800

    韩冲

--- app/src/main/java/com/bawei/myapplication/MainActivity.java
+++ app/src/main/java/com/bawei/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //韩冲
         initialize();
         iv.setBackgroundResource(R.drawable.ic_launcher);
         hd.setVisibility(View.INVISIBLE);
